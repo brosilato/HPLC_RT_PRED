9144ae5d67a73251e2ede455c4bec6d4f7a3e44a
brosilato <a.varela-alvarez@outlook.com>
2026-06-21 20:50:52 -0500

    processed the dataset so it includes SMILES strings

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+from rdkit import Chem
+
+def inchi_to_smiles(inchi_string: str) -> str | None:
+    """Converts InChI strings into SMILES one at a time
+
+    Args:
+        str (inchi_string): InChI string of the molecule
+
+    Returns:
+        str: Corresponding SMILES string of the molecule
+    """
+    mol = Chem.inchi.MolFromInchi(inchi_string)
+    if mol is None:
+        return None
+    smiles_string = Chem.MolToSmiles(mol)
+    return smiles_string
